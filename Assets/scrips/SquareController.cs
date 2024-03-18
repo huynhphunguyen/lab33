a08b86a252fe15c8ede41c59730adc5827cde70e
huynhphunguyen <nguyenhppd10647@gmail.com>
2024-03-18 16:00:44 +0700

    first commit

---
+++ Assets/scrips/SquareController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SquareController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float timeRemaining = 60;
+    public Text countdownText;
+    void Start()
+    {
+        StartCoroutine(Countdown());
+
+
+    }
+    IEnumerator Countdown()
+    {
+        while (timeRemaining > 0)
+        {
+            yield return new WaitForSeconds(1);
+            timeRemaining--;
+            countdownText.text = "Time " + timeRemaining.ToString();
+
+        }
+        countdownText.text = "Time's up!";
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, vertical, 0f).normalized;
+        transform.Translate(movement * 5f * Time.deltaTime);
+    }
+    public void LoadNextSccene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("Circle"))
+        {
+            Debug.Log("xxxx");
+            Vector2 firstPosition = new Vector2(-8, 1);
+            transform.position = firstPosition;
+        }
+        if (collision.gameObject.name.Equals("Box"))
+        {
+            Debug.Log("Win");
+            LoadNextSccene();
+        }    
+    }
+
+}
