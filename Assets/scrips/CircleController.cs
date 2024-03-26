069343411e9f7b22831a2cb854491d6407654839
huynhphunguyen <nguyenhppd10647@gmail.com>
2024-03-26 13:18:43 +0700

    agm1

--- Assets/scrips/CircleController.cs
+++ Assets/scrips/CircleController.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CircleController: MonoBehaviour
+public class CircleController : MonoBehaviour
 {
-    // Start is called before the first frame update
     private int direction = 1;
     private float moveSpeed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
     // Update is called once per frame
     void Update()
     {
         Vector3 movement = new Vector3(0f, direction, 0f);
-        transform.Translate(movement*moveSpeed*Time.deltaTime);
-        if (transform.position.y >=1.5f || transform.position.y <= -1.6f)
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+        if (transform.position.y > 1.5f || transform.position.y < -1.8f)
         {
             direction *= -1;
         }
